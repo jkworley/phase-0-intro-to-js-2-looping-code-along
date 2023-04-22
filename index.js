1a699d8b35d2e8c3fcf2da890824cb1b94887d5e
Justin Worley <jkworley@gmail.com>
2023-04-22 15:29:01 -0400

    commit tree 7b3e3cb43eb1848fbaeb05810b67b676be3e0dde

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const cards = [];
+
+function writeCards(cardFor, eventName) {
+    for (let i = 0; i < cardFor.length; i ++) {
+        const finishedCards = `Thank you, ${cardFor[i]}, for the wonderful ${eventName} gift!`;
+        cards.push(finishedCards);
+    }
+
+    return cards;
+};
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i--);
+    }
+};
